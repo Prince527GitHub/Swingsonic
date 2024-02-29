aa17869246e6c709c30a4200090d6f5370343cfc
Prince527 <Prince527gaming@gmail.com>
2024-02-29 09:47:55 -0400

    Update index.js

--- index.js
+++ index.js
@@ -7,21 +7,41 @@ const app = express();
 const xmlbuilder = require('xmlbuilder');
 
 function convertToXml(jsonObj) {
-    const xml = xmlbuilder.create(Object.keys(jsonObj)[0]);
+    const rootKey = Object.keys(jsonObj)[0];
+    const xml = xmlbuilder.create(rootKey);
 
     function convertToXmlObj(obj, parent) {
         for (const key in obj) {
-            if (typeof obj[key] === 'object') {
-                const child = parent.ele(key);
+            if (Array.isArray(obj[key])) {
+                obj[key].forEach((item, index) => {
+                    const validKey = convertToValidXmlName(`${key}_${index}`);
+                    const child = parent.ele(validKey);
+                    if (typeof item === 'object') {
+                        convertToXmlObj(item, child);
+                    } else {
+                        child.text(item);
+                    }
+                });
+            } else if (typeof obj[key] === 'object') {
+                const validKey = convertToValidXmlName(key);
+                const child = parent.ele(validKey);
                 convertToXmlObj(obj[key], child);
-            } else parent.att(key, obj[key]);
+            } else {
+                const validKey = convertToValidXmlName(key);
+                parent.att(validKey, obj[key]);
+            }
         }
-    };
+    }
 
-    convertToXmlObj(jsonObj[Object.keys(jsonObj)[0]], xml);
+    convertToXmlObj(jsonObj[rootKey], xml);
 
     return xml.end({ pretty: true });
-};
+}
+
+function convertToValidXmlName(name) {
+    name = name.replace(/^[^a-zA-Z_]+/, '_');
+    return name.replace(/[^a-zA-Z0-9_]/g, '_');
+}
 
 function shuffleArray(array) {
     for (let i = array.length - 1; i > 0; i--) {
