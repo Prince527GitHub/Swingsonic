f0b9b1adb0f35c19f69e57af34358d25be79ef49
Prince527 <Prince527gaming@gmail.com>
2025-03-05 18:56:56 -0400

    Patch proxy

--- packages/proxy.js
+++ packages/proxy.js
@@ -1,10 +1,26 @@
 const axios = require("axios");
 
-module.exports = async(res, req, url) => {
-    const response = await axios.get(url, { responseType: 'stream', headers: { "Cookie": req.user } });
+module.exports = async (res, req, url) => {
+    try {
+        const headers = {
+            "Cookie": req.user,
+            "User-Agent": req.headers["user-agent"] || "Mozilla/5.0",
+            "Accept": req.headers["accept"] || "*/*",
+            "Connection": "keep-alive",
+        };
 
-    const headers = ["content-type", "content-length", "content-disposition"];
-    headers.forEach(header => response.headers[header] && res.set(header, response.headers[header]));
+        if (req.headers["range"]) headers["Range"] = req.headers["range"];
 
-    response.data.pipe(res);
-} 
+        const response = await axios.get(url, {
+            responseType: "stream",
+            headers,
+        });
+
+        res.writeHead(response.status, response.headers);
+
+        response.data.pipe(res);
+    } catch (error) {
+        console.error("Proxy error:", error.message);
+        res.status(500).send("Error proxying request.");
+    }
+};
