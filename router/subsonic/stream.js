7a6fb5b000ec73c60f820cd261aee8885163e41e
Prince527 <Prince527gaming@gmail.com>
2025-03-09 22:05:49 -0300

    Update stream.js

--- router/subsonic/stream.js
+++ router/subsonic/stream.js
@@ -3,7 +3,5 @@ module.exports = async(req, res, proxy, xml) => {
 
     const decoded = JSON.parse(Buffer.from(decodeURIComponent(id), "base64").toString("utf-8"));
 
-    console.log(decoded)
-
     proxy(res, req, `${global.config.music}/file/${decoded.id}/legacy?filepath=${encodeURIComponent(decoded.path)}&container=mp3&quality=original`);
 }
