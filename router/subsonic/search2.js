c5efd22294faab2b6787fae2499f14342e9a9653
Prince527 <Prince527gaming@gmail.com>
2025-03-09 22:50:15 -0300

    Subsonic: Error handling

--- router/subsonic/search2.js
+++ router/subsonic/search2.js
@@ -6,59 +6,71 @@ module.exports = async(req, res, proxy, xml) => {
     let artists = [];
 
     if (artistCount >= 1 && query) {
-        artists = await (await fetch(`${global.config.music}/search/?itemtype=artists&q=${query}&start=${artistOffset || '0'}&limit=${artistCount || '50'}`, {
-            headers: {
-                "Cookie": req.user
-            }
-        })).json();
-        artists = artists.results.map(artist => ({
-            id: artist.artisthash,
-            name: artist.name
-        }));
+        try {
+            artists = await (await fetch(`${global.config.music}/search/?itemtype=artists&q=${query}&start=${artistOffset || '0'}&limit=${artistCount || '50'}`, {
+                headers: {
+                    "Cookie": req.user
+                }
+            })).json();
+            artists = artists.results.map(artist => ({
+                id: artist.artisthash,
+                name: artist.name
+            }));
+        } catch (error) {
+            console.log(error);
+        }
     }
 
     let albums = [];
 
     if (albumCount >= 1 && query) {
-        albums = await (await fetch(`${global.config.music}/search/?itemtype=albums&q=${query}&start=${albumOffset || '0'}&limit=${albumCount || '50'}`, {
-            headers: {
-                "Cookie": req.user
-            }
-        })).json();
-        albums = albums.results.map(album => ({
-            id: album.albumhash,
-            parent: album.albumhash,
-            title: album.title,
-            artist: album.albumartists[0].name,
-            isDir: "true",
-            coverArt: Buffer.from(JSON.stringify({ type: "album", id: album.image })).toString("base64")
-        }));
+        try {
+            albums = await (await fetch(`${global.config.music}/search/?itemtype=albums&q=${query}&start=${albumOffset || '0'}&limit=${albumCount || '50'}`, {
+                headers: {
+                    "Cookie": req.user
+                }
+            })).json();
+            albums = albums.results.map(album => ({
+                id: album.albumhash,
+                parent: album.albumhash,
+                title: album.title,
+                artist: album.albumartists[0].name,
+                isDir: "true",
+                coverArt: Buffer.from(JSON.stringify({ type: "album", id: album.image })).toString("base64")
+            }));
+        } catch (error) {
+            console.log(error);
+        }
     }
 
     let tracks = [];
 
     if (songCount >= 1 && query) {
-        tracks = await (await fetch(`${global.config.music}/search/?itemtype=tracks&q=${query}&start=${songOffset || '0'}&limit=${songCount || '50'}`, {
-            headers: {
-                "Cookie": req.user
-            }
-        })).json();
-        tracks = tracks.results.map(track => ({
-            id: track.trackhash,
-            parent: track.albumhash,
-            title: track.title,
-            isDir: false,
-            album: track.album,
-            artist: track.albumartists[0].name,
-            track: 0,
-            year: 2024,
-            genre: "Unknown",
-            coverArt: Buffer.from(JSON.stringify({ type: "album", id: track.image })).toString("base64"),
-            size: 0,
-            contentType: "audio/mpeg",
-            suffix: "mp3",
-            isVideo: false
-        }));
+        try {
+            tracks = await (await fetch(`${global.config.music}/search/?itemtype=tracks&q=${query}&start=${songOffset || '0'}&limit=${songCount || '50'}`, {
+                headers: {
+                    "Cookie": req.user
+                }
+            })).json();
+            tracks = tracks.results.map(track => ({
+                id: track.trackhash,
+                parent: track.albumhash,
+                title: track.title,
+                isDir: false,
+                album: track.album,
+                artist: track.albumartists[0].name,
+                track: 0,
+                year: 2024,
+                genre: "Unknown",
+                coverArt: Buffer.from(JSON.stringify({ type: "album", id: track.image })).toString("base64"),
+                size: 0,
+                contentType: "audio/mpeg",
+                suffix: "mp3",
+                isVideo: false
+            }));
+        } catch (error) {
+            console.log(error);
+        }
     }
 
     const json = {
