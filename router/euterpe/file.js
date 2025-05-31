5b97c9fb16d16336e0597f8ff140fa00a5c355dd
Prince527 <Prince527gaming@gmail.com>
2025-05-31 10:17:23 -0300

    Fix Euterpe & Update Packages

--- router/euterpe/file.js
+++ router/euterpe/file.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const proxy = require("../../packages/proxy");
 
 router.get("/:id", async(req, res) => {
-    const id = req.query.id;
+    const id = req.params.id;
 
     const decoded = JSON.parse(Buffer.from(decodeURIComponent(id), "base64").toString("utf-8"));
 
