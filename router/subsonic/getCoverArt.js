4eb21aa522bf0104001d39ef04161298f5d5ec1b
TikhonP <tikhon.petrishchev@gmail.com>
2025-12-13 18:06:13 +0300

    Fix coverArt enpoint for opensubsonic api

--- router/subsonic/getCoverArt.js
+++ router/subsonic/getCoverArt.js
@@ -1,7 +1,21 @@
+function safeDecodeId(id) {
+    try {
+        const json = Buffer.from(id, "base64").toString("utf-8");
+        return JSON.parse(json);
+    } catch {
+        return null;
+    }
+}
+
 module.exports = async(req, res, proxy, xml) => {
     const id = req.query.id;
 
-    const decode = JSON.parse(Buffer.from(id, "base64").toString("utf-8"));
+    const decoded = safeDecodeId(id);
 
-    proxy(res, req, `${global.config.music}/img/${decode.type === "artist" ? "artist" : "thumbnail"}/medium/${decode.id}`);
+    if (decoded) {
+        proxy(res, req, `${global.config.music}/img/${decoded.type === "artist" ? "artist" : "thumbnail"}/medium/${decoded.id}`);
+    } else {
+        // обычный Subsonic coverArt id
+        proxy(res, req, `${global.config.music}/img/thumbnail/medium/${encodeURIComponent(id)}`);
+    }
 }
